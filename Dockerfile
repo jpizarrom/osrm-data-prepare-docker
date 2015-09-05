870451763f67abdd2cd5570a2a53969349369423
Juan Pizarro <jpizarrom@gmail.com>
2015-09-05 01:47:35 -0300

    upload to s3

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,14 @@ FROM jpizarrom/osrm-docker:v4.7.0
 
 MAINTAINER Juan Pizarro "jpizarrom@gmail.com"
 
+
+RUN apt-get install -yqq unzip && curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip" && unzip awscli-bundle.zip && sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
 ADD solo-osrm-data-chile.json /chef/
 
 ADD myapp /chef/cookbooks/myapp
+
+ENV STORAGE_FLAVOR local
 
 ADD run.sh /opt/
 
--- run.sh
+++ run.sh
@@ -6,3 +6,7 @@ chef-solo -c /chef/solo.rb -j /chef/solo-osrm-data-chile.json
 
 # TODO: compress
 tar cjvf /tmp/data.tar.bz2 -C /opt/osrm-data/chile/bicycle/ chile-latest.osrm.edges chile-latest.osrm.geometry chile-latest.osrm.names chile-latest.osrm.ramIndex chile-latest.osrm.fileIndex chile-latest.osrm.hsgr chile-latest.osrm.nodes
+
+if [[ "$STORAGE_FLAVOR" == "s3" ]]; then
+   aws s3 cp /tmp/data.tar.bz2 s3://$AWS_S3_BUCKET/bicycle-`date +"%Y-%m-%d-%H-%M-%S"`.tar.bz2
+fi
