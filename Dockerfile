81f4ba6881eec295584d917bb235c2e690651495
Juan Pizarro <jpizarrom@gmail.com>
2015-09-05 11:32:33 -0300

    custom target and profile

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,18 @@ FROM jpizarrom/osrm-docker:v4.7.0
 
 MAINTAINER Juan Pizarro "jpizarrom@gmail.com"
 
-
 RUN apt-get install -yqq unzip && curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip" && unzip awscli-bundle.zip && sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
-ADD solo-osrm-data-chile.json /chef/
+ADD solo-osrm-data-prepare.json /chef/
 
 ADD myapp /chef/cookbooks/myapp
 
+ENV OSRM_VERSION 4.7.0
+
 ENV STORAGE_FLAVOR local
+ENV CHEF_SOLO_JSON_PATH /chef/solo-osrm-data-prepare.json
+ENV REGION chile
+ENV PROFILE bicycle
 
 ADD run.sh /opt/
 
--- run.sh
+++ run.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-chef-solo -c /chef/solo.rb -j /chef/solo-osrm-data-chile.json
-
-# TODO: compress
-tar cjvf /tmp/data.tar.bz2 -C /opt/osrm-data/chile/bicycle/ chile-latest.osrm.edges chile-latest.osrm.geometry chile-latest.osrm.names chile-latest.osrm.ramIndex chile-latest.osrm.fileIndex chile-latest.osrm.hsgr chile-latest.osrm.nodes
+chef-solo -c /chef/solo.rb -j $CHEF_SOLO_JSON_PATH
 
 if [[ "$STORAGE_FLAVOR" == "s3" ]]; then
-   aws s3 cp /tmp/data.tar.bz2 s3://$AWS_S3_BUCKET/bicycle-`date +"%Y-%m-%d-%H-%M-%S"`.tar.bz2
+   tar cjvf /tmp/data.tar.bz2 -C /opt/osrm-data/$REGION/$PROFILE/ $REGION-latest.osrm.edges $REGION-latest.osrm.geometry $REGION-latest.osrm.names $REGION-latest.osrm.ramIndex $REGION-latest.osrm.fileIndex $REGION-latest.osrm.hsgr $REGION-latest.osrm.nodes
+   aws s3 cp /tmp/data.tar.bz2 s3://$AWS_S3_BUCKET/$REGION-$PROFILE-`date +"%Y-%m-%d-%H-%M-%S"`-$OSRM_VERSION.tar.bz2
 fi
