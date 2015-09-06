ae8988598c5cee140ea59ecafe47967ea8757470
Juan Pizarro <jpizarrom@gmail.com>
2015-09-05 21:51:41 -0300

    fix compress local file

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ set -e
 
 chef-solo -c /chef/solo.rb -j $CHEF_SOLO_JSON_PATH
 
+tar cjvf /tmp/data.tar.bz2 -C /opt/osrm-data/$REGION/$PROFILE/ $REGION-latest.osrm.edges $REGION-latest.osrm.geometry $REGION-latest.osrm.names $REGION-latest.osrm.ramIndex $REGION-latest.osrm.fileIndex $REGION-latest.osrm.hsgr $REGION-latest.osrm.nodes
+
 if [[ "$STORAGE_FLAVOR" == "s3" ]]; then
-   tar cjvf /tmp/data.tar.bz2 -C /opt/osrm-data/$REGION/$PROFILE/ $REGION-latest.osrm.edges $REGION-latest.osrm.geometry $REGION-latest.osrm.names $REGION-latest.osrm.ramIndex $REGION-latest.osrm.fileIndex $REGION-latest.osrm.hsgr $REGION-latest.osrm.nodes
    aws s3 cp /tmp/data.tar.bz2 s3://$AWS_S3_BUCKET/$REGION-$PROFILE-`date +"%Y-%m-%d-%H-%M-%S"`-$OSRM_VERSION.tar.bz2
 fi
