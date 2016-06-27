88571f877c2b4c529fea911575dca16bcbf69de0
jpizarrom <jpizarrom@gmail.com>
2016-06-27 01:45:41 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,13 +8,19 @@ fi
 
 if [[ "$DATA_COMPRESS" == "bz2" ]]; then
    tar cjvf /tmp/data.tar.bz2 -C /opt/osrm-data/$REGION/$PROFILE/ \
+       $REGION$REGION_FILENAME_SUFFIX.osrm.core \
+       $REGION$REGION_FILENAME_SUFFIX.osrm.datasource_names \
+       $REGION$REGION_FILENAME_SUFFIX.osrm.datasource_indexes \
        $REGION$REGION_FILENAME_SUFFIX.osrm.edges \
        $REGION$REGION_FILENAME_SUFFIX.osrm.fileIndex \
        $REGION$REGION_FILENAME_SUFFIX.osrm.geometry \
        $REGION$REGION_FILENAME_SUFFIX.osrm.hsgr \
+       $REGION$REGION_FILENAME_SUFFIX.osrm.icd \
        $REGION$REGION_FILENAME_SUFFIX.osrm.names \
        $REGION$REGION_FILENAME_SUFFIX.osrm.nodes \
-       $REGION$REGION_FILENAME_SUFFIX.osrm.ramIndex
+       $REGION$REGION_FILENAME_SUFFIX.osrm.properties \
+       $REGION$REGION_FILENAME_SUFFIX.osrm.ramIndex \
+       $REGION$REGION_FILENAME_SUFFIX.osrm.timestamp
 fi
 
 if [[ "$STORAGE_FLAVOR" == "s3" ]]; then
